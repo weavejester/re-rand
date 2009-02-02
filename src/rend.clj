57e9f137ba4e394a58ba8071f05711a5a7104b52
weavejester <jreeves@weavejester.com>
2009-02-02 01:07:55 +0000

    Added comments and copyrights

--- src/rend.clj
+++ src/rend.clj
@@ -1,7 +1,20 @@
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+;; rend
+;;
+;; Generate random strings that match the supplied regular expression.
+
 (ns rend
   (:use rend.parser.rules))
 
 (defn rend
+  "Returns a random string that matches the regular expression."
   [re]
   (let [[parsed remainder] (pattern (str re))]
     (if (empty? remainder)
--- src/rend/parser/rules.clj
+++ src/rend/parser/rules.clj
@@ -1,3 +1,16 @@
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+;; rend.parser.rules:
+;;
+;; Rules to parse a regular expression into a series of string generating
+;; functions.
+
 (ns rend.parser.rules
   (:use rend.parser.tools)
   (:use [clojure.set :only (difference)]))
