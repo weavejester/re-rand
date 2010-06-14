40a3f0e4f362627d85de386feba834a85e80c979
weavejester <jreeves@weavejester.com>
2010-06-15 00:10:32 +0100

    Added Clojars dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject re-rand "0.1.0"
   :description "Generates random strings from regular expressions"
-  :dependencies [[org.clojure/clojure "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]])
